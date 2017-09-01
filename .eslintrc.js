19b6d9c9285db3fd5aad3dc425ae9221541f0a0e
Jan Buschtöns <jan@buschtoens.me>
2017-09-01 16:11:16 +0200

    chore(eslint): add pretiter, ember, ember-suave

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,26 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:ember-suave/recommended',
+    'prettier'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'ember/named-functions-in-promises': ['error', { allowSimpleArrowFunction: true }],
+    'ember-suave/no-const-outside-module-scope': 'off',
+    'prefer-const': 'error'
   }
 };
